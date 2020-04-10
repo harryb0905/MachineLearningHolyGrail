1e8e40feb2306b57a0921ab1f316ffd001319b31
harryb0905 <harryb0905@googlemail.com>
2020-04-10 13:07:40 +0100

    Added functions for reading files into pandas DataFrames

---
+++ ml-api/fileopts/_input.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import csv
+
+def read_file_as_df(filepath, delimiter=','):
+  return pd.read_csv(filepath, delimiter=delimiter)
+
+def write_df(df, filepath, delimiter=','):
+  df.to_csv(filepath, index=False)
+
+def main():
+  filepath = '../datasets/people.csv'
+  people_df = read_file_as_df(filepath)
+  print(people_df.head())
+
+  # Write list of rows to CSV
+  rows = [
+    ["9000", "24.99"],
+    ["9250", "32.99"]
+  ]
+  columns = ["ItemId", "Price"]
+  items_df = pd.DataFrame(rows, columns=columns)
+
+  filepath = 'items.csv'
+  # result = write_df(items_df, filepath, rows)
+
+  # Write list of dicts to CSV
+  rows = [
+    {'player_name': 'Magnus Carlsen', 'fide_rating': 2870},
+    {'player_name': 'Fabiano Caruana', 'fide_rating': 2822},
+    {'player_name': 'Ding Liren', 'fide_rating': 2801}
+  ]
+  columns = ['player_name', 'fide_rating']
+  players_df = pd.DataFrame(rows, columns=columns)
+
+  filepath = 'players.csv'
+  # result = write_df(players_df, filepath, rows)
+
+if __name__ == "__main__":
+  main()
