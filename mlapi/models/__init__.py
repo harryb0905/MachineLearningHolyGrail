e09a3bfd9ec07563d210a460698ddf09c2e5d7bc
harryb0905 <harryb0905@googlemail.com>
2020-04-12 13:32:44 +0100

    Added Resources to API in app.py

--- mlapi/models/__init__.py
+++
@@ -1,9 +0,0 @@
-from ._logistic_regression import LogisticRegressionWrapper
-from ._svc import SVCWrapper
-from ._random_forest import RandomForestWrapper
-
-__all__ = [
-  LogisticRegressionWrapper,
-  SVCWrapper,
-  RandomForestWrapper
-]
