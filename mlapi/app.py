6353efc16a6dc8b34cfb521294823ea54104ba40
harryb0905 <harryb0905@googlemail.com>
2020-04-12 16:07:32 +0100

    Added boston house price test model to api

--- mlapi/app.py
+++ mlapi/app.py
@@ -14,6 +14,7 @@ parser.add_argument('query')
 # Setup the Api resource routing here - route the URL to the resource
 api.add_resource(resources.Models, '/models')
 api.add_resource(resources.Model, '/models/<string:model_name>')
+api.add_resource(resources.TestModel, '/models/test_model')
 app.register_blueprint(api_bp)
 
 if __name__ == '__main__':
